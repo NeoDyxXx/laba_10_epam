09152a35f4d4223641a4ab56ef464dce4f72bb74
NDX <yakirik3@gmail.com>
2021-12-14 23:54:03 +0300

    set artefact

--- TestsArchitecture/TestsArchitecture/Tests/CrazyTests.cs
+++ TestsArchitecture/TestsArchitecture/Tests/CrazyTests.cs
@@ -25,50 +25,56 @@ namespace TestsArchitecture
             Driver.DriverInstance.CloseBrowser();
         }
 
-        [Test]
-        public void PushCorrectStockTransactAndCheckInHistoryList()
-        {
-            steps.LoginInQuotex();
-            steps.ChooseStockInList("AUD/CAD");
-            steps.PushStockTransaction(new System.DateTime(2021, 1, 1, 0, 1, 0), 50, true);
+        //[Test]
+        //public void PushCorrectStockTransactAndCheckInHistoryList()
+        //{
+        //    steps.LoginInQuotex();
+        //    steps.ChooseStockInList("AUD/CAD");
+        //    steps.PushStockTransaction(new System.DateTime(2021, 1, 1, 0, 1, 0), 50, true);
 
             
-            foreach(var item in steps.GetListItemsInHistoryList())
-            {
-                TestContext.WriteLine(item);
-            }
-            Assert.IsTrue(Utils.GetStandartValueForTest.GetCorrectList().EqualListsOfItemInHistoryList(steps.GetListItemsInHistoryList()));
-        }
+        //    foreach(var item in steps.GetListItemsInHistoryList())
+        //    {
+        //        TestContext.WriteLine(item);
+        //    }
+        //    Assert.IsTrue(Utils.GetStandartValueForTest.GetCorrectList().EqualListsOfItemInHistoryList(steps.GetListItemsInHistoryList()));
+        //}
+
+        //[Test]
+        //public void CheckTypeZerosFromTimeAndCostInput()
+        //{
+        //    steps.LoginInQuotex();
+        //    steps.PushStockTransaction(new System.DateTime(2021, 1, 1, 0, 0, 0), 0, false);
+
+        //    Assert.AreNotEqual(Utils.GetStandartValueForTest.GetZerosValueTimeAndCost().Time, steps.GetTimeAndCostOfStockTransaction().Time);
+        //    Assert.AreNotEqual(Utils.GetStandartValueForTest.GetZerosValueTimeAndCost().Cost, steps.GetTimeAndCostOfStockTransaction().Cost);
+        //}
+
+        //[Test]
+        //public void CheckTypeNegativeValueForCostAndNotCorrectForTime()
+        //{
+        //    steps.LoginInQuotex();
+        //    steps.PushStockTransaction(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Time,
+        //        System.Convert.ToInt32(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Cost));
+
+        //    Assert.AreNotEqual(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Time, steps.GetTimeAndCostOfStockTransaction().Time);
+        //    Assert.AreNotEqual(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Cost, steps.GetTimeAndCostOfStockTransaction().Cost);
+        //}
+
+        //[Test]
+        //public void MultiPushStockTransaction()
+        //{
+        //    steps.LoginInQuotex();
+        //    steps.ChooseStockInList("AUD/CAD");
+        //    steps.PushStockTransaction(new System.DateTime(2021, 1, 1, 0, 1, 0), 50, true, 10);
+
+        //    Assert.IsTrue(Utils.GetStandartValueForTest.GetMultiClickList().EqualListsOfItemInHistoryList(steps.GetListItemsInHistoryList()));
+        //}
 
         [Test]
-        public void CheckTypeZerosFromTimeAndCostInput()
+        public void TestFailureTest()
         {
-            steps.LoginInQuotex();
-            steps.PushStockTransaction(new System.DateTime(2021, 1, 1, 0, 0, 0), 0, false);
-
-            Assert.AreNotEqual(Utils.GetStandartValueForTest.GetZerosValueTimeAndCost().Time, steps.GetTimeAndCostOfStockTransaction().Time);
-            Assert.AreNotEqual(Utils.GetStandartValueForTest.GetZerosValueTimeAndCost().Cost, steps.GetTimeAndCostOfStockTransaction().Cost);
-        }
-
-        [Test]
-        public void CheckTypeNegativeValueForCostAndNotCorrectForTime()
-        {
-            steps.LoginInQuotex();
-            steps.PushStockTransaction(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Time,
-                System.Convert.ToInt32(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Cost));
-
-            Assert.AreNotEqual(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Time, steps.GetTimeAndCostOfStockTransaction().Time);
-            Assert.AreNotEqual(Utils.GetStandartValueForTest.GetNegativeValueFromCostAndNotCorrectFromTime().Cost, steps.GetTimeAndCostOfStockTransaction().Cost);
-        }
-
-        [Test]
-        public void MultiPushStockTransaction()
-        {
-            steps.LoginInQuotex();
-            steps.ChooseStockInList("AUD/CAD");
-            steps.PushStockTransaction(new System.DateTime(2021, 1, 1, 0, 1, 0), 50, true, 10);
-
-            Assert.IsTrue(Utils.GetStandartValueForTest.GetMultiClickList().EqualListsOfItemInHistoryList(steps.GetListItemsInHistoryList()));
+            Assert.IsTrue(false);
         }
     }
 }
